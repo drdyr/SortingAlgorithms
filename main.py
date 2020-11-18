0ef678247d1e8d1948c048279f3c1caf186830a7
seb <seb.hallx@gmail.com>
2020-11-18 00:16:46 +0000

    merge sort added

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 def quicksort(arr):
     # base case for recursion
     if len(arr) <= 1:
@@ -32,4 +34,37 @@ def selection_sort(arr):
     return arr
 
 def merge_sort(arr):
-    
+    def merge(a,b): # helper function merging two different sorted arrays
+        c = [] # final sorted array
+        # start at beginning of both sorted arrays
+        a_idx, b_idx = 0, 0
+
+        # compare item from each list and add smaller item to merged list
+        while a_idx < len(a) and b_idx < len(b):
+            if a[a_idx] < b[b_idx]:
+                c.append(a[a_idx])
+                a_idx += 1
+            else:
+                c.append(b[b_idx])
+                b_idx += 1
+
+        # if a empty, add the rest of b and vice-versa
+        if a_idx == len(a):
+            c += b[b_idx:]
+        else:
+            c += a[a_idx:]
+        return c
+
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr)//2
+
+    left, right = merge_sort(arr[:mid]), merge_sort(arr[mid:])
+
+    return merge(left, right)
+
+
+x = [random.randint(0,100) for _ in range(50)]
+
+print(merge_sort(x))
